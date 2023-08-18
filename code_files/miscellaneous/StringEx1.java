8c2dc33c5cdd38006f3d8ff0b3af2840ff12075b
AwareSelf <namratamarathe81@gmail.com>
2023-08-18 15:35:44 +0530

    today's class examples - array, string, static, pass by reference
    today's class examples - array, string, static, pass by reference

--- code_files/miscellaneous/StringEx1.java
+++ code_files/miscellaneous/StringEx1.java
@@ -1,4 +1,7 @@
 package miscellaneous;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringEx1 {
 
@@ -52,7 +55,52 @@ public class StringEx1 {
 	        String str7 = "\t    Namrata    \u2005";
 	        System.out.println("xxx"+str7.stripTrailing()+"xxx");
 		
-
+        System.out.println("checking String isBlank and isEmpty..");
+	        String b = "";
+	        System.out.println(b.isBlank());
+	        System.out.println(b.isEmpty());
+	        
+	        b = "    ";
+	        System.out.println("xxxx"+b+"xxx");
+	        System.out.println(b.isBlank());
+	        System.out.println(b.isEmpty());
+	        
+	        String multi = "Hi All \n This is java \n aaa \n bbb";
+	        System.out.print(multi);
+	        
+	       Stream<String> stream = multi.lines();
+	     //  System.out.println(stream.count());
+	       
+	       System.out.println("\n print all lines of multiline string..");
+	       //collect all the values in a stream and add them to List
+	       // and return that list.
+	       List<String> lst = stream.collect(Collectors.toList());
+	       System.out.println(lst);
+	       System.out.println(lst.get(1));
+	       
+	       String s = "java ";
+	       s = s.repeat(5);
+	       System.out.println(s);
+	       
+	       double price = 12.4;
+	       int no = (int)price;
+	       
+	       //using wrapper classes for conversion
+	       Double pricee = price; // Double pricee = new Double(price); --autoboxing
+	       
+	       no = pricee.intValue();
+	        System.out.println(no);
+	      
+	       //to convert double-string to double
+	       String val ="12.4";
+	       double v = Double.parseDouble(val);
+	       System.out.println(v);
+	       
+	       //to convert int-string to int
+	       String val1="10";
+	       int nx = Integer.parseInt(val1);
+	       nx = nx+1;
+	       System.out.println(nx);
 	}
 
 }
